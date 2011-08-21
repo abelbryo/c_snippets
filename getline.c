5bc22ad0e0555addac8a01cd5ea2650b1972f36f
Abel Terefe <abelbryo@gmail.com>
2011-08-22 00:47:38 +0300

    Initial commit

---
+++ getline.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXCHARS 	1000
+
+int getline(char *line, int max);
+
+int main(int argc, char *argv[]){
+	char line[MAXCHARS];
+	int count = getline(line, MAXCHARS);
+	printf("Input was %s\n", line);
+	printf("Length: %d\n", count-1);
+	
+	
+	
+	return 0;
+}
+
+/**
+ * Reads user input
+ * @param line, a pointer to a char
+ * @param max, the maximum number of chars allowed
+ * @return, the length of the string plus one.
+ */
+int getline(char *line, int max){
+	int c, count = 0;
+	while((c = getchar())!= EOF && c != '\n'){
+		if(count < max){
+			*(line++) = c;
+			count++;
+		}
+	}
+	*(line) = '\0';
+	
+	return count+1; // The plus one accounts for the null character which
+}					// is appended at the end of the string.
